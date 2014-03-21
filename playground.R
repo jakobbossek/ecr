1f5d196067444dedbc324e6cbc0c864aeebc98ec
Jakob Bossek <info@jakobbossek.de>
2014-03-21 12:04:56 +0100

    add rudimentary logging of the optimization trace, add todo list (thinking aboud conceptual things)

--- playground.R
+++ playground.R
@@ -1,6 +1,56 @@
 library(soobench)
-library(helpr)
+library(JBmisc)
 library(BBmisc)
+library(ggplot2)
+
+# TODO
+# - think about genral structure of function
+# - think about what type of elements should be wrapped in S3 classes:
+#   population (has size, individuals), individual (has fitness value, date of birth, param values)
+#   optimization trace (data frame with best individuals of each iteration)
+# - what types of traget funs should be supported? Just funs with numeric-only parameters like the
+#   soobnech funs or allow discrete params as well?
+# - check Bernds ParamHelpers package thorougly. Maybe makeOptPath, all the make...Param funs can
+#   be used in this package.
+# - research for common (and not so common) mutation, selection and recombination operators
+# - investigate existing R packages for evolutionary optimization (RFreak, GA, ...)
+# - think about the best programmatical interface solution for the user. It should be as simple as
+#   possible to extend the functionality and write propriatary mutation, selection ... funs.
+# - at all costs follow the "DRY" principle, i.e., "don't repeat yourself"
+# - 
+
+# IDEAS
+# - allow chaining of mutators, i. e., allow to apply more than one mutator to the offspring
+
+
+makeTrace = function(n.params, param.names, target.name) {
+  # this stuff sucks
+  if (missing(param.names)) {
+    param.names = paste("x", seq_len(n.params), sep = "")
+  }
+  if (missing(target.name)) {
+    target.name = "y"
+  }
+  col.names = c(param.names, target.name, "generation")
+  return(structure(
+    list(
+      trace = data.frame(),
+      col.names = col.names,
+      param.names = param.names,
+      target.name = target.name,
+      size = 1L
+      ),
+    class = "esooTrace"))
+}
+
+addToTrace = function(trace, individual, generation) {
+  tmp = c(individual$individual, individual$fitness, generation)
+  tmp = as.data.frame(t(tmp))
+  colnames(tmp) = trace$col.names
+  trace$trace = rbind(trace$trace, tmp)
+  return(trace)
+}
+
 
 generateRandomInitialPopulation = function(size, n.params, lower.bounds, upper.bounds) {
   design = matrix(0, nrow = size, ncol = n.params)
@@ -8,6 +58,15 @@ generateRandomInitialPopulation = function(size, n.params, lower.bounds, upper.b
     design[, i] = runif(size, min = lower.bounds[i], max = upper.bounds[i])
   }
   return(design)
+}
+
+getBestIndividual = function(population, population.fitness) {
+  best.idx = which.min(population.fitness)
+  best.fitness = population.fitness[best.idx]
+  best.individual = population[best.idx, ]
+  return(structure(
+    list(fitness = best.fitness, individual = best.individual),
+    class = "esooIndividual"))
 }
 
 computeFitness = function(individuals, fitness.fun) {
@@ -69,10 +128,17 @@ correctBounds = function(individuals, lower.bounds, upper.bounds) {
 }
 
 esoo = function(f, max.iter, mu) {
-  population = generateRandomInitialPopulation(mu, number_of_parameters(f), lower_bounds(f), upper_bounds(f))
+  # maybe make use of Bernds makeOptPathDf and ParamHelpers for this.
+
+  n = number_of_parameters(f)
+  population = generateRandomInitialPopulation(mu, n, lower_bounds(f), upper_bounds(f))
   catf("Initial Population generated.")
   print(population)
   population.fitness = computeFitness(population, f)
+  best = getBestIndividual(population, population.fitness)
+  trace = makeTrace(n)
+  trace = addToTrace(trace, best, 0)
+
   catf("Fitness of start population:")
   print(population.fitness)
   i = 1L
@@ -97,22 +163,34 @@ esoo = function(f, max.iter, mu) {
     population = res$population
     population.fitness = res$fitness
 
+    best = getBestIndividual(population, population.fitness)
+    trace = addToTrace(trace, best, i)
+
     i = i + 1
     #if (i == 3)
       #stopf("debug")
   }
-  best.idx = which.min(population.fitness)
-  best.fitness = population.fitness[best.idx]
-  best.individual = population[best.idx, ]
   return(
     structure(list(
-      best.param = best.individual,
-      best.fitness = best.fitness
+      best.param = best$individual,
+      best.fitness = best$fitness,
+      trace = trace
       ), class = "esoor")
   )
 }
 
+plotTrace = function(trace) {
+  trace = trace$trace
+  pl = ggplot(data = trace, aes(x = generation, y = y))
+  pl = pl + geom_line()
+  pl = pl + ggtitle("Optimization trace.")
+  pl = pl + xlab("Generation") + ylab("Best fitness value")
+  print(pl)
+}
+
 sphere_fun = generate_sphere_function(2)
-res = esoo(sphere_fun, 50, 50)
+
+res = esoo(sphere_fun, 50, 200)
+plotTrace(res$trace)
 
 
