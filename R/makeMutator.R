bd4e09c79d6d1d13b20d113c23ce78b7d74104f1
Jakob Bossek <info@jakobbossek.de>
2014-04-09 16:01:00 +0200

    add generator for mutation operators

---
+++ R/makeMutator.R
@@ -0,0 +1,30 @@
+# Helper function which constructs a mutator, i. e., a mutation operator.
+#
+# @param mutator [\code{function}]\cr
+#   Actual mutation operator.
+# @param name [\code{character(1)}]\cr
+#   Name of the mutator.
+# @param supported [\code{character}]\cr
+#   Vector of strings/names of supported parameter representations. For example
+#   'permutation', 'float', 'binary'.
+# @return [\code{esoo_mutator}]
+#   Mutator object.
+makeMutator = function(mutator, name, supported = getAvailableRepresentations()) {
+  checkArg(name, cl = "character", len = 1L, na.ok = FALSE)
+  checkArg(supported, subset = getAvailableRepresentations(), na.ok = FALSE)
+
+  #FIXME: there is no checkArg option in BBmisc to check if argument is a function!
+  if (!is.function(mutator)) {
+    stopf("Mutator must be a function.")
+  }
+  attr(mutator, "name") = name
+  attr(mutator, "supported") = supported
+  #FIXME: in BBmisc addClasses should have an option not to add already existent classes
+  mutator = addClasses(mutator, "esoo_mutator")
+  return(mutator)
+}
+
+# Helper function which returns all supported parameter representations.
+getAvailableRepresentations = function() {
+  c("permutation", "binary", "float", "integer")
+}
