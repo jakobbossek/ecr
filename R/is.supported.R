4ed5fa519950335f28fdb24548de6f77b9dfaf16
Jakob Bossek <info@jakobbossek.de>
2014-04-14 14:02:35 +0200

    add helper method is.supported

---
+++ R/is.supported.R
@@ -0,0 +1,18 @@
+#' Helper function to check if esoo operator supports given representation.
+#'
+#' @param [\code{esoo_operator}]\cr
+#'   Object of type \code{esoo_operator}.
+#' @param [\code{character(1)}]\cr
+#'   Representation as a string.
+#' @return [\code{logical(1)}]
+#'   \code{TRUE}, if operator supports the representation type.
+#'
+#' @export
+is.supported = function(operator, representation) {
+  UseMethod("is.supported")
+}
+
+#' @S3method is.supported esoo_operator
+is.supported.esoo_operator = function(operator, representation) {
+  return (representation %in% getSupportedRepresentations(operator))
+}
