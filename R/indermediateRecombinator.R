65e6fcffa243062313abb297958dd01d1afb59c7
Jakob Bossek <info@jakobbossek.de>
2014-04-14 15:00:16 +0200

    add intermediate recombinator for float representation

---
+++ R/indermediateRecombinator.R
@@ -0,0 +1,30 @@
+# Generator of the indermediate recombination operator.
+#
+# @param individuals [\code{setOfIndividuals}]\cr
+#   Set of individuals.
+# @param type [\code{character(1)}]\cr
+#   String indicating the type of recombination used. Until now only the default value
+#   intermediate is implemented.
+# @param params [\code{list}]\cr
+#   Further arguments for the recombination procedure (for example weights for intermediate recombination).
+# @return [\code{setOfIndividuals}]
+#   Recombined offspring.
+makeIntermediateRecombinator = function() {
+  # Helper function for recombination of individuals.
+  #
+  # @param setOfIndividuals [\code{setOfIndividuals}]\cr
+  #   Set of individuals.
+  # @param control [\code{list}]\cr
+  #   Control object.
+  # @return [\code{setOfIndividuals}]
+  #   Recombined offspring.
+  intermediateRecombinator = function(setOfIndividuals, control=list()) {
+    child = apply(setOfIndividuals$population, 2, sum) / 2
+    makePopulation(t(as.matrix(child)))
+  }
+
+  makeMutator(
+    mutator = intermediateRecombinator,
+    name = "Intermediate recombinator",
+    supported = c("float"))
+}
