e865128b0a286ffd0c61fd876b316835bc5e4a3e
Jakob Bossek <info@jakobbossek.de>
2014-04-14 14:18:26 +0200

    update main function with new interface

--- R/esoo.R
+++ R/esoo.R
@@ -4,10 +4,8 @@
 #'
 #' @param f [\code{function}]\cr
 #'   Target function.
-#' @param max.iter [\code{integer(1)}]\cr
-#'   Maximum number of iterations.
-#' @param pop.size [\code{integer(1)}]\cr
-#'   Population size.
+#' @param control [\code{esoo.control}]\cr
+#'   Control object.
 #' return [\code{esooResult}]
 #'   Object of type \code{esooResult} containing a list:
 #'   \itemize{
@@ -15,24 +13,17 @@
 #'    \item{best.value \code{numeric(1)}}{Best reached value.}
 #'    \item{trace \code{esooTrace}}{Optimization path.}
 #'   }
-#' @examples
-#' \dontrun{
-#' library(soobench)
-#' f = generate_sphere_function(2)
-#' res = esoo(f, max.iter = 50, pop.size = 10)
-#' print(res$best.value)
-#' }
 #' @export
-esoo = function(f, max.iter, pop.size) {
-  #FIXME: add sanity checks
-  #FIXME: add esooControl object. This object should be responsable to extensively sanity check
-  #       all the input parameters and check the parameter combinations for validity
-
+esoo = function(f, control) {
   #FIXME: until now only soobnech funs are supported. Force the user to offer a special type of
   #       funs (esoo_fitness_function)?
-  n = number_of_parameters(f)
+  n = control$n.params
+  max.iter = control$max.iter
+  population.size = control$population.size
   #FIXME: generation of initial population is based on the type of parameters
-  population = generateRandomInitialPopulation(pop.size, n, lower_bounds(f), upper_bounds(f))
+  print(n)
+  print(population.size)
+  population = generateRandomInitialPopulation(population.size, n, lower_bounds(f), upper_bounds(f))
   #FIXME: add show.info/monitoring option to allow the used (de)activation of messages
   #       Look for a logging package in R
   catf("Initial Population generated.")
@@ -52,14 +43,14 @@ esoo = function(f, max.iter, pop.size) {
     #FIXME: until now only one child generated
     #FIXME: recombination, mutation and so on are all based on the representation of the individuals
     children = recombinate(parents, type = "intermediate")
-    children = gaussMutation(children)
+    children = control$mutator(children, control)
     children = correctBounds(children, lower_bounds(f), upper_bounds(f))
 
     children = computeFitness(children, f)
     population = mergePopulations(population, children)
 
     #FIXME: elitism, survival of the fittest if (mu, lambda) strategy is used
-    population = selectForSurvival(population, pop.size, strategy = "mupluslambda")
+    population = selectForSurvival(population, population.size, strategy = "mupluslambda")
 
     best = getBestIndividual(population)
     trace = addToTrace(trace, best, i)
