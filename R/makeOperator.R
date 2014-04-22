18f19088d5f44f2551edaf0b7450d6dd998f03f9
Jakob Bossek <info@jakobbossek.de>
2014-04-22 17:07:52 +0200

    better sanity checks for makeOperator

--- R/makeOperator.R
+++ R/makeOperator.R
@@ -10,13 +10,10 @@
 # @return [\code{ecr_operator}]
 #   Operator object.
 makeOperator = function(operator, name, supported = getAvailableRepresentations()) {
+  checkArg(operator, cl = "function")
   checkArg(name, cl = "character", len = 1L, na.ok = FALSE)
   checkArg(supported, subset = getAvailableRepresentations(), na.ok = FALSE)
 
-  #FIXME: there is no checkArg option in BBmisc to check if argument is a function!
-  if (!is.function(operator)) {
-    stopf("operator must be a function.")
-  }
   attr(operator, "name") = name
   attr(operator, "supported") = supported
 
