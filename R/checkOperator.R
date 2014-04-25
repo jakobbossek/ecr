e1e67431918d62df69d92acf4addc7e79e0fd311
Jakob Bossek <info@jakobbossek.de>
2014-04-25 10:19:58 +0200

    fix stupid programming issue

--- R/checkOperator.R
+++ R/checkOperator.R
@@ -4,20 +4,20 @@
 #   Operator to check.
 checkOperator = function(operator) {
   checkArg(operator, "ecr_operator")
-  checkArg(attr(operator$name), "character", len = 1L, na.ok = FALSE)
-  checkArg(attr(operator$description), "character", len = 1L, na.ok = FALSE)
-  checkArg(attr(operator$supported), "character", min.len = 1L, na.ok = FALSE)
+  checkArg(attr(operator, "name"), "character", len = 1L, na.ok = FALSE)
+  checkArg(attr(operator, "description"), "character", len = 1L, na.ok = FALSE)
+  checkArg(attr(operator, "supported"), "character", min.len = 1L, na.ok = FALSE)
 }
 
 checkMutator = function(mutator) {
   checkOperator(mutator)
   checkArg(mutator, "ecr_mutator")
-  checkArg(attr(mutator$defaults), "list", na.ok = FALSE)
+  checkArg(attr(mutator, "defaults"), "list", na.ok = FALSE)
 }
 
 checkRecombinator = function(recombinator) {
   checkOperator(recombinator)
   checkArg(recombinator, "ecr_recombinator")
-  checkArg(attr(recombinator$n.parents), "integer", len = 1L, lower = 2L, na.ok = FALSE)
-  checkArg(attr(recombinator$defaults), "list", na.ok = FALSE)
+  checkArg(attr(recombinator, "n.parents"), "integer", len = 1L, lower = 2L, na.ok = FALSE)
+  checkArg(attr(recombinator, "defaults"), "list", na.ok = FALSE)
 }
