17ac93005e16af33cf4ac956627ad5d0b811aa4f
Jakob Bossek <info@jakobbossek.de>
2014-05-07 14:19:44 +0200

    add helper getOperatorCheckFunction

---
+++ R/getOperatorCheckFunction.R
@@ -0,0 +1,15 @@
+#' Returns the parameter check function of a given operator.
+#'
+#' @param operator [\code{ecr_operator}]\cr
+#'   Operator object.
+#' @return [\code{character(1)}]
+#'   Function for parameter check.
+#' @export
+getOperatorCheckFunction = function(operator) {
+  UseMethod("getOperatorCheckFunction")
+}
+
+#' @S3method getOperatorCheckFunction ecr_operator
+getOperatorCheckFunction.ecr_operator = function(operator) {
+  attr(operator, "checkFunction")
+}
