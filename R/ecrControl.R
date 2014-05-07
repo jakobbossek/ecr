b989a25892feee3571c94b8de1a9b8b56478bb74
Jakob Bossek <info@jakobbossek.de>
2014-05-07 12:55:29 +0200

    replace %is% checks be inherits

--- R/ecrControl.R
+++ R/ecrControl.R
@@ -96,7 +96,7 @@ ecr.control = function(
   checkArg(show.info.stepsize, cl = "integer", len = 1L, lower = 1, na.ok = FALSE)
   checkArg(mutator.control, cl = "list", na.ok = FALSE)
   checkArg(recombinator.control, cl = "list", na.ok = FALSE)
-  if (!(monitor %is% "ecr_monitor")) {
+  if (!inherits(monitor, "ecr_monitor")) {
     stopf("Currently only monitor of type 'ecr_monitor' supported")
   }
 
