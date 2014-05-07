7dc54f31436bdcb96632222f8db32e33bedadf5f
Jakob Bossek <info@jakobbossek.de>
2014-05-07 11:59:13 +0200

    Close #30, i. e., provide helper function which returns the ParamSet of a soo function

---
+++ R/helpers.R
@@ -0,0 +1,24 @@
+#' Generates ParamSet object out of a given soobench function.
+#'
+#' @param fn [\code{\link[soobench]{soo_function}}]\cr
+#'   Source function.
+#' @return [\code{\link[ParamHelpers]{ParamSet}}]
+#'   Parameter set of type \code{\link[ParamHelpers]{ParamSet}}
+#' @examples
+#' library(soobench)
+#' fn = generate_ackley_function(8)
+#' par.set = extractParamSetFromSOOFunction(fn)
+#' print(par.set)
+#' @export
+extractParamSetFromSOOFunction = function(fn) {
+  if (!is_soo_function(fn)) {
+    stop("Function must be of type soo_function from soobench package.")
+  }
+  makeNumericParamSet(
+    len = number_of_parameters(fn),
+    id = "x",
+    lower = lower_bounds(fn),
+    upper = upper_bounds(fn),
+    vector = FALSE
+  )
+}
