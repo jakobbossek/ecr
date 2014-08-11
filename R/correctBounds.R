c15bd7a580f756b47f7a020ef020eea298b3a68d
Jakob Bossek <info@jakobbossek.de>
2014-08-11 08:14:05 +0200

    refactor correctBounds

--- R/correctBounds.R
+++ R/correctBounds.R
@@ -2,20 +2,12 @@
 #
 # @param individuals [\code{setOfIndividuals}]\cr
 #   Set of individuals, for example the entire population.
-# @param lower.bounds [\code{numerci}]\cr
-#   Vector of lower bounds for each dimension.
-# @param upper.bounds [\code{numerci}]\cr
-#   Vector of upper bounds for each dimension.
+# @param par.set [\code{\link[ParamHelpers]{ParamSet}]\cr
+#   Parameter set.
 # @return [\code{setOfIndividuals}].
-correctBounds = function(individuals, lower.bounds, upper.bounds) {
+correctBounds = function(individuals, par.set) {
   for (i in 1:nrow(individuals$population)) {
-    for (j in 1:ncol(individuals$population)) {
-      if (individuals$population[i, j] < lower.bounds[j]) {
-        individuals$population[i, j] = lower.bounds[j]
-      } else if (individuals$population[i, j] > upper.bounds[j]) {
-        individuals$population[i, j] = upper.bounds[j]
-      }
-    }
+    individuals$population[i, ] = repairPoint(par.set, as.list(individuals$population[i, ]))
   }
   return(individuals)
 }
