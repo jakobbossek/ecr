db5fc66e456a66b88ca783e2f16e9c3e5779ee2d
Jakob Bossek <info@jakobbossek.de>
2015-02-13 15:10:15 +0100

    add log.fitness parameter to autoplot

--- R/autoplotECRResult.R
+++ R/autoplotECRResult.R
@@ -6,11 +6,13 @@
 #'   Lower and upper bound for generation. If \code{NULL}, this is set automatically.
 #' @param ylim [\code{numeric(2)} | NULL]\cr
 #'   Lower and upper bound for fitness. If \code{NULL}, this is set automatically.
+#' @param log.fitness [\code{logical(1)}]\cr
+#'   Log-transform fitness values? Default is \code{FALSE}.
 #' @param ... [any]\cr
 #'   Not used.
 #' @return [\code{\link[ggplot2]{ggplot}}]
 #' @export
-autoplot.ecr_result = function(object, xlim = NULL, ylim = NULL, ...) {
+autoplot.ecr_result = function(object, xlim = NULL, ylim = NULL, log.fitness = FALSE, ...) {
   ggdf = as.data.frame(object$opt.path)
   ggdf = ggdf[c("dob", "pop.min.fitness", "pop.mean.fitness", "pop.median.fitness", "pop.max.fitness")]
 
@@ -18,6 +20,7 @@ autoplot.ecr_result = function(object, xlim = NULL, ylim = NULL, ...) {
   ylim = BBmisc::coalesce(ylim, c(0, max(ggdf$pop.max.fitness)))
   assertNumeric(ylim, len = 2L, any.missing = FALSE)
   assertNumeric(xlim, len = 2L, any.missing = FALSE)
+  assertFlag(log.fitness)
 
   requirePackages("reshape2", why = "ecr")
   ggdf = melt(ggdf, c("dob"))
@@ -29,6 +32,11 @@ autoplot.ecr_result = function(object, xlim = NULL, ylim = NULL, ...) {
   pl = pl + xlim(xlim) + ylim(ylim)
   pl = pl + scale_linetype_discrete(name = "Type")
   pl = pl + ggtitle(sprintf("Optimization trace for function '%s'", getName(object$objective.fun)))
+
+  if (log.fitness) {
+    pl = pl + scale_y_log10()
+    pl = pl + ylab("log(Fitness)")
+  }
 
   return(pl)
 }
