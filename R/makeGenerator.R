1a6995d3548193948565e908220606f286d66344
Jakob Bossek <info@jakobbossek.de>
2015-04-14 17:44:55 +0200

    add and export makeGenerator

---
+++ R/makeGenerator.R
@@ -0,0 +1,23 @@
+#' Helper function which constructs a generator, i. e., a function which generates
+#' an initial population.
+#
+#' @param generator [\code{function}]\cr
+#'   Actual generator function. Should expect the size of the population \code{size}
+#'   as a first and a ecr control object \code{control} as the second argument.
+#' @param name [\code{character(1)}]\cr
+#'   Name of the generator.
+#' @param description [\code{character(1)}]\cr
+#'   Short description of how the generator works.
+#' @param supported [\code{character}]\cr
+#'   Vector of strings/names of supported parameter representations, i.e.,
+#'   'permutation', 'float', 'binary' or 'custom'.
+#' @return [\code{ecr_generator}]
+#'   Generator object.
+#' @export
+makeGenerator = function(generator, name, description,
+  supported = getAvailableRepresentations()) {
+  assertFunction(generator, args = c("size", "control"), ordered = TRUE)
+  generator = makeOperator(generator, name, description, supported)
+  generator = addClasses(generator, c("ecr_generator"))
+  return(generator)
+}
