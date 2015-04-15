a72bd9e44ad6f96fce96de45af8539d2bf71e6e1
Jakob Bossek <info@jakobbossek.de>
2015-04-15 15:21:39 +0200

    add k-tournament selector

---
+++ R/selector.k-tournament.R
@@ -0,0 +1,36 @@
+#' Generator for k-Tournament selection.
+#'
+#' Selects genes for the mating pool. It works as follows: k individuals from the
+#' population are chosen randomly and the best one is selected to be included into
+#' the mating pool. This process is repeated until the desired number of individuals
+#' for the mating pool is reached.
+#'
+#' @param k [\code{integer(1)}]\cr
+#'   Number of individuals to participate in each tournament. Default is \code{2L}.
+#' @return [\code{setOfIndividuals}]
+#' @export
+makeTournamentSelector = function(k = 3L) {
+  force(k)
+
+  selector = function(population, n.mating.pool) {
+    inds = population$individuals
+    fitness = population$fitness
+    n.population = length(fitness)
+    pop.idx = seq(n.population)
+
+    idx = integer(n.mating.pool)
+    for (i in seq(n.mating.pool)) {
+      # choose k individuals at random ...
+      competitor.idx = sample(pop.idx, size = k)
+      # ... and store the best
+      idx[i] = competitor.idx[which.max(fitness[competitor.idx])]
+    }
+
+    return(makePopulation(inds[idx], fitness[idx]))
+  }
+  makeSelector(
+    selector = selector,
+    name = "k-Tournament selector",
+    description = "Select k individuals at random, choose the best, repeat until mating pool is filled."
+  )
+}
