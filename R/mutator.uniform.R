503ae7f4dbe85ba2a52201d69de0cf2e849c5cd9
Jakob Bossek <info@jakobbossek.de>
2015-04-16 09:35:02 +0200

    add Uniform mutator

---
+++ R/mutator.uniform.R
@@ -0,0 +1,27 @@
+#' Generator of the Uniform mutation operator.
+#'
+#' This mutation operator works on real-valued genotypes only. It selects a position
+#' in the solution vector at random and replaced it with a uniformally chosen value
+#' within the box constraints of the corresponding parameter.
+#' This mutator may proof beneficial in early stages of the optimization process,
+#' since it distributes points widely within the box constraints and thus may
+#' hinder premature convergence. However, in later stages - when fine tuning is
+#' necessary, this feature is disadvantegous.
+#'
+#' @return [\code{ecr_mutator}]
+#' @export
+makeUniformMutator = function() {
+  mutator = function(ind, args = list(), control) {
+    n.params = length(ind)
+    idx = sample(n.params, size = 1L)
+    ind[idx] = runif(1L, min = control$par.lower, max = control$par.upper)
+    return(ind)
+  }
+
+  makeMutator(
+    mutator = mutator,
+    name = "Uniform mutator",
+    description = "Replaces a randomly chosen parameter with a random value within the bounds",
+    supported = "float"
+  )
+}
