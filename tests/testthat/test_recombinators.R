5f5e479a1f597edddddbb1ef80c2d6981a452bcf
Jakob Bossek <info@jakobbossek.de>
2015-04-16 12:18:34 +0200

    add test for permutation recombinators

---
+++ tests/testthat/test_recombinators.R
@@ -0,0 +1,27 @@
+context("recombination operators (recombinators)")
+
+test_that("recombinators for permutations work as expected", {
+  # defs
+  n = 10L # permutation length
+  n.reps = 5L # number of repetitions
+  expected = seq(n) # sequence we expect offspring to be a permutation of
+
+  # check validity of produced output for each permutation-based recombinator
+  for (recombinatorGenerator in c(makePMXRecombinator)) {
+    recombine = recombinatorGenerator()
+    # generate sample permutations
+    parent1 = sample(n)
+    parent2 = sample(n)
+
+    for (i in seq(n.reps)) {
+      child = recombine(list(parent1, parent2))
+      # check that the child is actually a permutation
+      expect_true(setequal(expected, child), info = sprintf(
+        "Offspring ('%s') is not a permutation of sequence (%s) for operator '%s'",
+        collapse(child),
+        collapse(expected),
+        getOperatorName(recombine))
+      )
+    }
+  }
+})
