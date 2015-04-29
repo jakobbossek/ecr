2feed4c8ca6d6f06b8b225559b86c74228cbc2b0
Jakob Bossek <info@jakobbossek.de>
2015-04-29 16:15:41 +0200

    update sms-emoa example

--- inst/examples/sms_emoa_example.R
+++ inst/examples/sms_emoa_example.R
@@ -1,5 +1,11 @@
-library(smoof)
-library(ecr)
+library(methods)
+library(testthat)
+library(devtools)
+library(BBmisc)
+library(ggplot2)
+
+set.seed(1)
+load_all(".")
 
 # THIS FILE IS WORK-IN-PROGRESS/EXPERIMENAL
 
@@ -7,7 +13,7 @@ library(ecr)
 #FIXME: integrate NondominatedSetSelector as ecr_selector
 #FIXME: we need to check in setupECRControl or in ecr if the operators can work
 # on multi-objective stuff
-#
+
 
 # Get set of dominated individuals.
 #
@@ -16,8 +22,7 @@ library(ecr)
 # @param fn [function]
 #   Fitness function.
 # @return [list]
-#FIXME: formulate this for matrices where each column describes one vector. However,
-# this is just a naive imolementation for testing purpose. Will be replaced
+#FIXME: formulate this for matrices where each column describes one vector
 getDominatedSet = function(x) {
   n = length(x)
 
@@ -60,31 +65,26 @@ makeNondominatedSetSelector = function() {
 }
 
 ctrl = setupECRControl(
-  n.population = 20L,
-  n.offspring = 10L,
+  n.population = 10L,
+  n.offspring = 1L,
   representation = "float",
-  monitor = makeConsoleMonitor()
+  monitor = makeConsoleMonitor(),
+  stopping.conditions = setupStoppingConditions(max.iter = 100L)
 )
 
 ctrl = setupEvolutionaryOperators(
   ctrl,
-  # tournamend and roulette wheel work on scalar fitness values only
-  # Thus, simply select two random elements for reproduction
   parent.selector = makeSimpleSelector(),
   mutator = makeGaussMutator(),
   recombinator = makeCrossoverRecombinator(),
-  # everything can be used, we just needed a multi-objective-specific survival selector
-  survivalSelector = makeNondominatedSetSelector()
+  survival.selector = makeNondominatedSetSelector()
 )
 
 obj.fn = smoof::makeZDT1Function(2L)
 
 res = doTheEvolution(obj.fn, ctrl)
 
-pl = smoof::visualizeParetoOptimalFront(obj.fn)
-pf = res$pareto.front
-pf = as.data.frame(t(pf))
-names(pf) = paste0("x", 1:2)
-pl = pl + geom_point(data = pf, aes(x = x1, y = x2), colour = "green")
-print(pl)
+pl = visualizeParetoOptimalFront(obj.fn)
+pf = as.data.frame(res$pareto.front)
+pl = pl + geom_point(data = pf, aes(x = y1, y = y2), colour = "green")
 stop("FIN")
