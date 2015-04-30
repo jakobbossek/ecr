99cb1a36d83c464d1fe50e074651e478860e1964
Jakob Bossek <info@jakobbossek.de>
2015-04-30 13:49:30 +0200

    add dominated, which.dominated, which.nondominated

--- R/dominates.R
+++ R/dominates.R
@@ -1,4 +1,8 @@
+# Helper functions for deciding and selecting no
+
 #' Check if a vector dominates another vector.
+#'
+#' @keywords optimize
 #'
 #' @param x [\code{numeric}]\cr
 #'   First vector.
@@ -9,4 +13,74 @@
 dominates = function(x, y) {
   stopifnot(length(x) == length(y))
   return(all(x <= y) && any(x < y))
+}
+
+#' Check whether a vector is dominated by another vector.
+#'
+#' @keywords optimize
+#'
+#' @param x [\code{numeric}]\cr
+#'   First vector.
+#' @param y [\code{numeric}]\cr
+#'   Second vector.
+#' @return [\code{logical(1)}] Returns \code{TRUE} if \code{x} is dominated by
+#'   \code{y} and \code{FALSE} otherwise.
+#' @export
+isDominated = function(x, y) {
+  return(dominates(y, x))
+}
+
+#' Check for pareto dominance.
+#'
+#' This function takes a numeric matrix \code{x} where each row corresponds to
+#' a point and returns a logical vector. The i-th position of the latter is
+#' \code{TRUE} if the i-th point is dominated by at least one other point.
+#'
+#' @param x [\code{matrix}]\cr
+#'   Numeric (n x d) matrix where n is the number of points and d is the number
+#'   of objectives.
+#' @return [\code{logical}]
+#' @export
+dominated = function(x) {
+  assertMatrix(x, min.rows = 2L, min.cols = 2L, any.missing = FALSE)
+  n = nrow(x)
+  dominated = logical(n)
+  for (i in seq.int(n)) {
+    for (j in seq.int(i, n)) {
+      dominated[i] = dominated[i] || isDominated(x[i, ], x[j, ])
+      dominated[j] = dominated[j] || isDominated(x[j, ], x[i, ])
+    }
+  }
+  return(dominated)
+}
+
+#' Determine which points of a set are not (non)dominated.
+#'
+#' Simple wrapper functions around \code{\link{dominated}}. Given a matrix with one
+#' point per row the \code{which.dominated} returns the row numbers of the dominated points
+#' and \code{which.nondominated} the row numbers of the nondominated points.
+#'
+#' @keywords optimize
+#'
+#' @param x [\code{matrix}]\cr
+#' Numeric (n x d) matrix where n is the number of points and d is the number
+#'   of objectives.
+#' @return [\code{integer}]
+#' @examples
+#' data(mtcars)
+#' # assume we want to maximize horsepower and minimize gas consumption
+#' cars = mtcars[, c("mpg", "hp")]
+#' cars$hp = -cars$hp
+#' idxs = which.nondominated(as.matrix(cars))
+#' print(mtcars[idxs, ])
+#' @rdname which.dominated
+#' @export
+which.dominated = function(x) {
+  return(which(dominated(x)))
+}
+
+#' @rdname which.dominated
+#' @export
+which.nondominated = function(x) {
+  return(which(!dominated(x)))
 }
--- tests/testthat/test_dominates.R
+++ tests/testthat/test_dominates.R
@@ -5,5 +5,33 @@ test_that("dominates and isDominated works as expected", {
   expect_true(isDominated(c(2, 3), c(1, 2)))
   expect_true(dominates(c(1, 2), c(1, 3)))
   expect_false(dominates(c(1, 2), c(1, 2)))
+  expect_false(dominates(c(1, 2), c(2, 1)))
+  expect_false(isDominated(c(1, 2), c(2, 1)))
   expect_false(dominates(c(843.3, 2313.3), c(42.2, 654.3324)))
+})
+
+test_that("[which.{non}]dominated works well on matrices", {
+  m = matrix(
+    c(# utopia point
+      1, 1, 1,
+      # dominated points
+      2, 2, 3,
+      2, 1, 2,
+      2, 6, 1),
+    byrow = TRUE, ncol = 3L
+  )
+  dom = dominated(m)
+  # check that there is boolean value for each point
+  expect_equal(length(dom), nrow(m))
+
+  # check that we really have three dominated points
+  expect_equal(sum(dom), 3L)
+
+  # get indizes of dominated and nondominated points
+  dom.idxs = which.dominated(m)
+  nondom.idxs = which.nondominated(m)
+
+  expect_true(setequal(dom.idxs, 2:4))
+  expect_true(nondom.idxs == 1L)
+  expect_true(setequal(c(dom.idxs, nondom.idxs), seq(nrow(m))))
 })
