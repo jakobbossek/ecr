3240004b8b7562622e65bbf5ed4007478a9a7310
Jakob Bossek <info@jakobbossek.de>
2015-06-15 18:10:23 +0200

    add function to compute hypervolume contribution

--- R/computeDominatedHypervolume.R
+++ R/computeDominatedHypervolume.R
@@ -1,26 +1,37 @@
-#' Computation of the dominated hypervolume.
+#' Functions for the calculation of the dominated hypervolume (contribution).
 #'
-#' Given a set of points \code{x}, this function computes the dominated hypervolume
-#' of the points regarding the reference point \code{ref.point}. If the latter
-#' is not provided, one is automatically determined by computing the maximum
-#' in each dimension.
+#' The function \code{computeDominatedHypervolume} computes the dominated
+#' hypervolume of a set of points given a reference set whereby
+#' \code{computeHypervolumeContribution} computes the hypervolume contribution
+#' of each point.
+#'
+#' If no reference point is given the nadir point of the set \code{x} is
+#' determined and a positive offset with default 1 is added. This is to ensure
+#' that the reference point dominates all of the points in the reference set.
 #'
 #' @param x [\code{matrix}]\cr
 #'   Matrix of points.
 #' @param ref.point [\code{numeric} | \code{NULL}]\cr
 #'   Reference point. Set to the maximum in each dimension by default if not provided.
-#' @return [\code{numeric(1)}] Dominated hypervolume.
+#' @param offset [\code{numeric(1)}]\cr
+#'   Offset to be added to each component of the reference point only in the case
+#'   where no reference is provided and one is calculated automatically.
+#' @return [\code{numeric(1)}] Dominated hypervolume in the case of
+#'  \code{computeDominatedHypervolume} and the dominated hypervolume of a single
+#'  point in the case of \code{computeHypervolumeContribution}.
 #' @export
+#' @rdname dominated_hypervolume
 computeDominatedHypervolume = function(x, ref.point = NULL) {
   # sanity checks
   assertMatrix(x, mode = "numeric", any.missing = FALSE)
-  if (any(is.infinite(x))) {
-    warningf("Set of points contains infinite %i values.", which(is.infinite(x)))
-    return(NaN)
-  }
 
   if (is.null(ref.point)) {
     ref.point = apply(x, 1, max)
+  }
+
+  if (any(is.infinite(x))) {
+    warningf("Set of points contains infinite %i values.", which(is.infinite(x)))
+    return(NaN)
   }
 
   if (length(ref.point) != nrow(x)) {
@@ -34,4 +45,17 @@ computeDominatedHypervolume = function(x, ref.point = NULL) {
   }
 
   return(.Call("computeDominatedHypervolumeC", x, ref.point))
+}
+
+#' @export
+#' @rdname dominated_hypervolume
+computeHypervolumeContribution = function(x, ref.point = NULL, offset = 1) {
+  if (is.null(ref.point)) {
+    ref.point = approximateNadirPoint(x) + offset
+  }
+  assertMatrix(x, mode = "numeric", any.missing = FALSE)
+  assertNumeric(ref.point, any.missing = FALSE)
+  assertNumber(offset, finite = TRUE, lower = 0)
+
+  return(.Call("computeDominatedHypervolumeContributionC", x, ref.point))
 }
