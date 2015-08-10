d21c715608bbc1716775eb7cb8874e42a76c91c2
Jakob Bossek <info@jakobbossek.de>
2015-08-10 16:28:20 +0200

    extend mutator tests

--- tests/testthat/test_mutators.R
+++ tests/testthat/test_mutators.R
@@ -6,13 +6,13 @@ test_that("mutation operators working on permutation genes create valid offsprin
   n.reps = 5L
 
   # gather all mutators for permutation representation
-  availableMutators = c(
+  available.mutators = c(
     makeSwapMutator, makeInversionMutator,
     makeInsertionMutator, makeScrambleMutator
   )
 
   # check validity of produced output for each permutation-based mutator
-  for (mutatorGenerator in availableMutators) {
+  for (mutatorGenerator in available.mutators) {
     mutate = mutatorGenerator() # no mutation control parameters to check here
     test.seq = sample(seq(100L), 10L, replace = FALSE)
     for (i in seq(n.reps)) {
@@ -22,4 +22,23 @@ test_that("mutation operators working on permutation genes create valid offsprin
         collapse(test.seq), collapse(perm.seq)))
     }
   }
+})
+
+test_that("mutation operators working on real-numbered representation create valid offspring", {
+  # defs
+  n.reps = 5L
+
+  available.mutators = c(makeUniformMutator, makeGaussMutator)
+
+  for (mutatorGenerator in available.mutators) {
+    mutate = mutatorGenerator()
+    test.seq = runif(5L)
+    control = list(par.lower = 0, par.upper = 1)
+    for (i in seq(n.reps)) {
+      mut.seq = mutate(test.seq, control = control)
+      expect_true(all(mut.seq >= 0 && mut.seq <= 1), info = sprintf("Mutator '%s' did not stick to the
+        box constraints! Input: (%s), Output: (%s)", getOperatorName(mutate), collapse(test.seq), collapse(mut.seq)))
+    }
+  }
+
 })
