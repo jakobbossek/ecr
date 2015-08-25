339869297c368c7eb98d70bf642fd612237726fb
Jakob Bossek <info@jakobbossek.de>
2015-08-25 14:29:35 +0200

    add sms-emoa, extend emoa test

--- tests/testthat/test_emoa.R
+++ tests/testthat/test_emoa.R
@@ -1,16 +1,44 @@
 context("Evolutionary Multi-Objective Algorithms")
 
 test_that("preimplemented EMOAs work well", {
-  fn = smoof::makeZDT1Function(dimensions = 2L)
-  res = nsga2(
-    makeOptimizationTask(fn),
-    n.population = 10L,
-    n.offspring = 3L,
-    max.evals = 100L
+
+  #FIXME: is something like this in BBmisc? Needed that multiple times now.
+  printList = function(l) {
+    ns = names(l)
+    pairs = sapply(ns, function(n) {
+      paste0(n, l[[n]], sep = "=")
+    })
+    collapse(pairs, ", ")
+  }
+
+  expect_is_pareto_approximation = function(pf, n.obj, algo, prob, algo.pars) {
+    info.suffix = sprintf("Algo '%s' failed on problem '%s' with params '%s'",
+      algo, prob, printList(algo.pars))
+    expect_equal(nrow(pf), length(which.nondominated(t(pf))), info = paste0(info.suffix, "Not all returned points are nondominated."))
+    expect_true(all(is.numeric(pf)), info = paste0(info.suffix, "Not all returned points are numeric."))
+    expect_equal(ncol(pf), n.obj, info = paste0(info.suffix, "Number of columns is not equal to the number of objectives."))
+  }
+
+  fns = list(
+    zdt1 = smoof::makeZDT1Function(dimensions = 2L),
+    zdt2 = smoof::makeZDT2Function(dimensions = 2L),
+    zdt3 = smoof::makeZDT3Function(dimensions = 2L)
   )
-  pf = res$pareto.front
-  print(pf)
-  expect_equal(nrow(pf), length(which.nondominated(t(pf))))
-  expect_true(all(is.numeric(pf)))
-  expect_equal(ncol(pf), getNumberOfObjectives(fn))
+  max.evals = 100L
+
+  for (emoa in c("nsga2", "smsemoa")) {
+    for (n.pop in c(5, 10, 15)) {
+      for (fn in names(fns)) {
+        res = do.call(emoa, list(
+          task = makeOptimizationTask(fns[[fn]]),
+          n.population = n.pop,
+          n.offspring = 5L,
+          max.evals = max.evals)
+        )
+        expect_is_pareto_approximation(res$pareto.front, 2L, emoa, fn,
+          list(n.pop = n.pop, n.offspring = 5L, max.evals = max.evals)
+        )
+      }
+    }
+  }
 })
