9f813d06f70715a470da1c52f2b604264c45541d
Jakob Bossek <info@jakobbossek.de>
2015-08-25 08:20:55 +0200

    add makeOptimizationTask function

---
+++ R/makeOptimizationTask.R
@@ -0,0 +1,75 @@
+#' @title
+#'   Creates an optimization task.
+#'
+#' @description
+#'   An optimization task consists of the fitness/objective function, the
+#'   number of objectives and the \dQuote{direction} of optimization, i.e.,
+#'   which objectives should be minimized/maximized.
+#'
+#' @param fun [\code{function} | \code{smoof_function}]\cr
+#'   Fitness/objective function.
+#' @param n.objectives [\code{integer(1)}]\cr
+#'   Number of objectives. This must be a positive integer value unless \code{fun}
+#'   is of type \code{smoof_function}.
+#' @param minimize [\code{logical}]\cr
+#'   A logical vector indicating which objectives to minimize/maximize. By default
+#'   all objectives are assumed to be minimized.
+#' @return [\code{ecr_optimization_task}]
+#' @export
+makeOptimizationTask = function(fun, n.objectives = NULL, minimize = NULL) {
+  #FIXME: what parameters do we force it to have?
+  assertFunction(fun)
+  if (isSmoofFunction(fun)) {
+    if (is.null(n.objectives)) {
+      n.objectives = getNumberOfObjectives(fun)
+    } else {
+      if (n.objectives != getNumberOfObjectives(fun)) {
+        stopf("Something went wrong! You passed a smoof function with %i objectives,
+          but set the number of objectives in the task to %i manually.",
+          getNumberOfObjectives(fun), n.objectives
+        )
+      }
+    }
+  }
+  !is.null(n.objectives) && assertInt(n.objectives, lower = 1L, na.ok = FALSE)
+  !is.null(minimize) && assertLogical(minimize, any.missing = FALSE)
+
+  if (is.null(minimize)) {
+    minimize = rep(TRUE, n.objectives)
+  }
+
+  if (n.objectives != length(minimize)) {
+    stopf("Number of objectives does not correspond to the length of the minimize argument.")
+  }
+
+  task = makeS3Obj(
+    fitness.fun = fun,
+    n.objectives = n.objectives,
+    minimize = minimize,
+    classes = c("ecr_optimization_task")
+  )
+
+  if (isSmoofFunction(fun)) {
+    task$par.set = getParamSet(fun)
+    task$par.lower = getLower(task$par.set, with.nr = TRUE)
+    task$par.upper = getUpper(task$par.set, with.nr = TRUE)
+  } else {
+    # dummy parameter set if the passed function if not of type smoof
+    task$par.set = makeParamSet(makeNumericParam("dummy", lower = 0, upper = 1))
+  }
+
+  return(task)
+}
+
+#' @export
+print.ecr_optimization_task = function(x, ...) {
+  catf("[ecr OPTIMIZATION TASK]")
+  n.obj = "Single-objective"
+  if (x$n.objectives == 2L) {
+    n.obj = "Bi-objective"
+  } else if (x$n.objectives >= 3L) {
+    n.obj = "Many-objective"
+  }
+  catf("Objective type: %s", n.obj)
+  catf("(min: %i, max: %i)", sum(x$minimize), sum(!x$minimize))
+}
