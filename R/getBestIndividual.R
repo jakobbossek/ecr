294ae6ffe950ad57d948ad888f218fa3fe122400
Jakob Bossek <info@jakobbossek.de>
2015-08-31 17:30:52 +0200

    refactor getBestIndividual

--- R/getBestIndividual.R
+++ R/getBestIndividual.R
@@ -3,18 +3,20 @@
 #'
 #' @param population [\code{setOfIndividuals}]\cr
 #'  Population.
-#' @return [\code{essoIndividual}]
-#'  Individual with best, i. e., lowest fitness value.
+#' @return [list]
+#'  List with the following entries
+#'  \describe{
+#'    \item{individual}{Genotype of the best individual.}
+#'    \item{fitness}{Fitness of the individual.}
+#'  }
 #' @export
 getBestIndividual = function(population) {
   fitness = population$fitness
   best.idx = getMinIndex(fitness)
-  best.fitness = fitness[best.idx]
-  best.individual = population$individuals[[best.idx]]
   return(
     list(
-      individual = best.individual,
-      fitness = best.fitness
+      individual = population$individuals[[best.idx]],
+      fitness = fitness[best.idx]
     )
   )
 }
