dfcf25de0e8ea15ee10c0c0df3b8303e0f0cda4f
Dirk Surmann <surmann@statistik.tu-dortmund.de>
2015-09-02 17:16:07 +0200

    export 'makePopulation' to create own generators

--- R/makePopulation.R
+++ R/makePopulation.R
@@ -2,11 +2,20 @@
 #'   Wrap individuals in a 'setOfIndividuals'.
 #'
 #' @param individuals [\code{matrix}]\cr
-#'   Matrix of individuals.
+#'   List of individuals.
 #' @param fitness [\code{matrix}]\cr
-#'   Matrix of fitness values for the individuals.
+#'   Matrix with one row of fitness values for the individuals.
+#'   Default is \code{NULL}.
 #' @return [\code{setOfIndividuals}]
+#' @export
 makePopulation = function(individuals, fitness = NULL) {
+  assertList(individuals, any.missing = FALSE)
+  if (!testNull(fitness)) {
+    assert(checkMatrix(fitness, any.missing = FALSE, nrows = 1, ncols = length(individuals))
+           , checkNumber(fitness)
+           )
+  }
+
   makeS3Obj(
     individuals = individuals,
     fitness = fitness,
