6e6a05c76006e0970acf4d01591eb656e2699d49
Dirk Surmann <surmann@statistik.tu-dortmund.de>
2015-09-02 16:24:31 +0200

    typo

--- inst/examples/1plus1_GA_example.R
+++ inst/examples/1plus1_GA_example.R
@@ -44,7 +44,7 @@ control = setupEvolutionaryOperators(
   control,
   generator = makeBinaryGenerator(),
   mutator = makeBitFlipMutator(mutator.flip.prob = 1 / n.params),
-  recombinator = makeCrossoverRecombinator(recombinator.crossover.prob = 0.8),
+  recombinator = makeCrossoverRecombinator(recombinator.crossover.prob = 0.8)
 )
 print(control)
 
