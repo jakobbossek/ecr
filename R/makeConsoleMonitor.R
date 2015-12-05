da52529ba0176c7c46a05828f277be9457f39d28
Jakob Bossek <info@jakobbossek.de>
2015-12-05 01:17:31 +0100

    Fix issue #131, added num.format argument to makeConsoleMonitor

--- R/makeConsoleMonitor.R
+++ R/makeConsoleMonitor.R
@@ -1,18 +1,24 @@
-#' @title
-#'   Simple stdout monitoring function.
+#' @title Simple stdout monitoring function.
 #'
 #' @description
-#'   This is the default monitoring function used by ecr. It simply outputs
-#'   the iteration as well as minimal, mean and maximal target values from the current
-#'   population.
+#' This is the default monitoring function used by ecr. It simply outputs
+#' the iteration as well as minimal, mean and maximal target values from the current
+#' population.
 #'
 #' @param show.info.stepsize [\code{integer(1)}]\cr
 #'   Adjust the stepsize of iterations with informative messages.
+#' @param num.format [\code{character(1)}]\cr
+#'   Number format for output of numeric parameters. See the details section of
+#'   the manual for \code{\link[base]{sprintf}} for details.
+#'   Default is \dQuote{\%g}.
 #' @return [\code{ecr_monitor}]
 #' @export
-makeConsoleMonitor = function(show.info.stepsize = 5L) {
-  force(show.info.stepsize)
+makeConsoleMonitor = function(show.info.stepsize = 5L, num.format = "%g") {
   assertInteger(show.info.stepsize, len = 1L, lower = 1L, upper = 100L, any.missing = FALSE)
+  assertString(num.format)
+
+  force(show.info.stepsize)
+  force(num.format)
 
   makeMonitor(
     before = function(envir = parent.frame()) {
@@ -23,8 +29,9 @@ makeConsoleMonitor = function(show.info.stepsize = 5L) {
       fitness = envir$population$fitness
       iter = envir$iter
       if ((iter %% show.info.stepsize) == 0L) {
-        catf("Iter %i | y (min: %0.2g, mean: %0.2g, max: %0.2g)",
-          iter, min(fitness), mean(fitness), max(fitness))
+        call.format = sprintf("Iter %s | y (min: %s, mean: %s, max: %s)", "%i", num.format, num.format, num.format)
+        print(call.format)
+        catf(call.format, iter, min(fitness), mean(fitness), max(fitness))
       }
     },
     after = function(envir = parent.frame()) {
