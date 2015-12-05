347b9d269578f10410c8de85eea8984e4012d589
Jakob Bossek <info@jakobbossek.de>
2015-12-05 15:22:48 +0100

    fix examples (some broken due to operator interface changes)

--- inst/examples/custom_example.R
+++ inst/examples/custom_example.R
@@ -8,27 +8,12 @@ load_all(".")
 
 set.seed(123)
 
-getPairwiseDistances = function(x) {
-  distances = c()
-  n = nrow(x)
-  for (i in 1:n) {
-    for (j in 1:n) {
-      if (i == j) {
-        next
-      }
-      d = sum(x[i, ] - x[j, ])^2
-      distances = c(distances, d)
-    }
-  }
-  return(distances)
-}
-
 # defs
 N = 15L
 
 #fitness = makeFitnessFunction(
 fitness = function(x, ...) {
-  dists = getPairwiseDistances(x)
+  dists = dist(x)
   return(1 / sum(dists)) # since we want to maximise here
 }
 
@@ -39,13 +24,14 @@ ctrl = setupECRControl(
   representation = "custom", # bypass everything
   survival.strategy = "plus",
   monitor = makeConsoleMonitor(),
+  custom.constants = list(N = N),
   stopping.conditions = list(makeMaximumIterationsStoppingCondition(max.iter = 1000L))
 )
 
 myGenerator = makeGenerator(
   generator = function(size, task, control) {
     makePopulation(lapply(seq(size), function(i) {
-      matrix(runif(N * 2L), ncol = 2L)
+      matrix(runif(control$custom.constants$N * 2L), ncol = 2L)
     }))
   },
   name = "Point generator",
@@ -54,10 +40,10 @@ myGenerator = makeGenerator(
 )
 
 myMutator = makeMutator(
-  mutator = function(x, mutator.control, control, task) {
-    idx = which(runif(nrow(x)) < 0.1)
-    x[idx, ] = matrix(runif(2 * length(idx)), ncol = 2)
-    return(x)
+  mutator = function(ind, task, control) {
+    idx = which(runif(nrow(ind)) < 0.1)
+    ind[idx, ] = matrix(runif(2 * length(idx)), ncol = 2)
+    return(ind)
   },
   name = "Point-Shift mutation",
   description = "Shift all points in a random direction",
@@ -65,8 +51,8 @@ myMutator = makeMutator(
 )
 
 myRecombinator = makeRecombinator(
-  recombinator = function(x, control, task) {
-    x[[1]]
+  recombinator = function(inds, task, control) {
+    inds[[1]]
   },
   name = "Convex-Combination recombinator",
   description = "Make a convex combination of the point coordinates",
--- inst/examples/onesAndZeros_example.R
+++ inst/examples/onesAndZeros_example.R
@@ -2,12 +2,9 @@
 # The optimum of the function:
 # the first vector contains only ones,
 # the second vector contains only zeros.
-
-# clear all
-rm(list = ls(all = TRUE))
 library(devtools)
 library(BBmisc)
-# load ecr
+
 load_all(".", reset = TRUE)
 
 # objective function
@@ -21,35 +18,38 @@ obj.fn = makeSingleObjectiveFunction(
     , makeIntegerVectorParam("y", len = 15L, lower = 0, upper = 1)
   )
 )
+
 makeOptimumAppearsStoppingCondition = function(opt.fitness = 0) {
   condition.fun = function(opt.path) {
     # i.best = getOptPathBestIndex(opt.path)
     min.fitness.vector = getOptPathCol(opt.path, "pop.min.fitness")
     return(min(min.fitness.vector) == 0)
   }
-  makeStoppingCondition(condition.fun
-                        , name = "OptimumAppeared"
-                        , message = sprintf("Optimum appeared in population.")
+  makeStoppingCondition(
+    condition.fun,
+    name = "OptimumAppeared",
+    message = sprintf("Optimum appeared in population.")
   )
 }
+
 control = setupECRControl(
-  n.population = 100L
-  , n.offspring = 100L
-  , n.mating.pool = 100L
-  , representation = "binary"
-  , save.population.at = c(0L, 10L, 20L, 30L, 40L, 50L, 60L)
-  , monitor = makeConsoleMonitor(1L)
-  , stopping.conditions = list(
-    # makeMaximumIterationsStoppingCondition(max.iter = 100L)
+  n.population = 100L,
+  n.offspring = 100L,
+  n.mating.pool = 100L,
+  representation = "binary",
+  save.population.at = c(0L, 10L, 20L, 30L, 40L, 50L, 60L),
+  monitor = makeConsoleMonitor(1L),
+  stopping.conditions = list(
     makeOptimumAppearsStoppingCondition()
   )
 )
+
 control = setupEvolutionaryOperators(
-  control
-  , parent.selector = makeRouletteWheelSelector()
-  , generator = makeBinaryGenerator()
-  , mutator = makeBitFlipMutator(p = 0.001)
-  , recombinator = makeCrossoverRecombinator(p = 0.7)
+  control,
+  parent.selector = makeRouletteWheelSelector(),
+  generator = makeBinaryGenerator(),
+  mutator = makeBitFlipMutator(p = 0.001),
+  recombinator = makeCrossoverRecombinator(p = 0.7)
 )
 
 # Show all column names in opt.path:
