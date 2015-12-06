40513cbd2a573e615449317069e150add168f4e6
Jakob Bossek <info@jakobbossek.de>
2015-12-06 23:00:16 +0100

    overwork roulette wheel selector (add offset parameter)

--- R/selector.roulettewheel.R
+++ R/selector.roulettewheel.R
@@ -1,31 +1,45 @@
 #' @title
-#'   Generator for Roulette-Wheel-Selection (fitness-proportional selection).
+#' Generator for Roulette-Wheel-Selection (fitness-proportional selection).
 #'
 #' @description
-#'   Selects genes for the mating pool. The chance of an individual to get selected
-#'   is proportional to its fitness, i.e., better individuals get a higher chance
-#'   to take part in the reproduction process. Low-fitness individuals however,
-#'   have a positive fitness as well.
+#' Selects genes for the mating pool. The chance of an individual to get selected
+#' is proportional to its fitness, i.e., better individuals get a higher chance
+#' to take part in the reproduction process. Low-fitness individuals however,
+#' have a positive fitness as well.
 #'
+#' @details
+#' Fitness proportional selection can be naturally applied to single objective
+#' maximization problems. However, negative fitness values can are problematic.
+#' The Roulette-Wheel selector thus works with the following heuristic: if
+#' negative values occur, the negative of the smallest fitness value is added
+#' to each fitness value. In this case to avoid the smallest shifted fitness
+#' value to be zero and thus have a zero probability of being selected an additional
+#' positive constant \code{offset} is added (see parameters).
+#'
+#' @param offset [\numeric(1)]\cr
+#'   In case of negative fitness values all values are shifted towards positive
+#'   values by adding the negative of the minimal fitness value. However, in this
+#'   case the minimal fitness value after the shifting process is zero. The
+#'   \code{offset} is a positive numeric value which is added additionally to each
+#'   shifted fitness value. This way even the individual with the smallest fitness
+#'   value has a positive porbability to be selected.
+#'   Default is \code{0.1}.
 #' @return [\code{setOfIndividuals}]
 #' @family selectors
 #' @export
-# FIMXE: add thorough description of the heuristic used (see below).
-makeRouletteWheelSelector = function() {
+makeRouletteWheelSelector = function(offset = 0.1) {
+  assertNumber(offset, na.ok = FALSE, lower = 0, finite = TRUE)
+
+  force(offset)
+
   selector = function(population, storage, task, n.select, control) {
-    #FIXME: We do minimization; Roulette-Wheel selection cannot be used in
-    # minimization in general, since low fitness values lead to low selection
-    # probabilities. Another problem are negative fitness values.
-    # Here we do the following: a) "shift to positive" by adding the minimal fitness
-    # value to the fitness vector and add the magic number m = 0.1, i.e., (-10, -5, 20)
-    # becomes (0.1, 5.1, 20) to avoid zero fitness. b) Proceed to the reciprocal
-    # (10, 1 / 5.1, 1 / 20)
-    # But how to choose the magic number and is this really ok?
     inds = population$individuals
     fitness = as.numeric(population$fitness)
+    # shift negative values
     if (any(fitness <= 0L)) {
-      fitness = fitness + abs(min(fitness)) + 0.1
+      fitness = fitness + abs(min(fitness)) + offset
     }
+    #FIXME: this selector supports maximization only at the moment
     fitness = 1 / fitness
     n.population = length(inds)
     prob = fitness / sum(fitness)
