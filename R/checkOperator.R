8bbac6c006f42e095f57e5c5fe2322daeef2d1e1
Jakob Bossek <info@jakobbossek.de>
2015-12-06 13:48:46 +0100

    update check functions for operators

--- R/checkOperator.R
+++ R/checkOperator.R
@@ -1,25 +1,31 @@
-# @title
-#   Checks operator attributes for validity
+# @title Checks operator attributes for validity
 #
 # @param operator
 #   Operator to check.
-# @return Nothing
 checkOperator = function(operator) {
   assertClass(operator, "ecr_operator")
   assertCharacter(attr(operator, "name"), len = 1L, any.missing = FALSE)
   assertCharacter(attr(operator, "description"), len = 1L, any.missing = FALSE)
   assertCharacter(attr(operator, "supported"), min.len = 1L, any.missing = FALSE)
-  assertList(attr(operator, "defaults"), any.missing = FALSE)
+  assertList(attr(operator, "params"), any.missing = FALSE)
 }
 
+# @title Checks mutator attributes for validity
+#
+# @param operator
+#   Operator to check.
 checkMutator = function(mutator) {
   checkOperator(mutator)
   assertClass(mutator, "ecr_mutator")
 }
 
+# @title Checks recombinator attributes for validity
+#
+# @param operator
+#   Operator to check.
 checkRecombinator = function(recombinator) {
   checkOperator(recombinator)
   assertClass(recombinator, "ecr_recombinator")
   assertInteger(attr(recombinator, "n.parents"), len = 1L, lower = 2L, any.missing = FALSE)
-  assertList(attr(recombinator, "defaults"), any.missing = FALSE)
+  assertFlag(attr(recombinator, "multiple.children"))
 }
