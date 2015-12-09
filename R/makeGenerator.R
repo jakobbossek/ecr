af75c70d7c372c24d7d7de07d8e2de99efa02aa5
Dirk Surmann <surmann@statistik.tu-dortmund.de>
2015-12-09 11:15:05 +0100

    add params to makeGenerator (see issue #140)

--- R/makeGenerator.R
+++ R/makeGenerator.R
@@ -12,16 +12,21 @@
 #'   Name of the generator.
 #' @param description [\code{character(1)}]\cr
 #'   Short description of how the generator works.
+#' @param params [\code{list}]\cr
+#'   Named list of the parameters the generator has been initialized with.
+#'   Default is the empty list.
 #' @param supported [\code{character}]\cr
 #'   Vector of strings/names of supported parameter representations, i.e.,
 #'   'permutation', 'float', 'binary' or 'custom'.
 #' @return [\code{ecr_generator}]
 #'   Generator object.
 #' @export
-makeGenerator = function(generator, name, description,
-  supported = getAvailableRepresentations()) {
+makeGenerator = function(
+  generator, name, description,
+  supported = getAvailableRepresentations(),
+  params = list()) {
   assertFunction(generator, args = c("size", "task", "control"), ordered = TRUE)
-  generator = makeOperator(generator, name, description, supported)
+  generator = makeOperator(generator, name, description, supported, params)
   generator = addClasses(generator, c("ecr_generator"))
   return(generator)
 }
