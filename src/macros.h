b236cf44ca74b255ca8dc01636fb9b3d3a7425d8
Jakob Bossek <info@jakobbossek.de>
2016-01-23 00:20:42 +0100

    add simulated binary crossover recombinator (in C)

--- src/macros.h
+++ src/macros.h
@@ -1,10 +1,11 @@
 #ifndef SEXP_MACROS
 #define SEXP_MACROS
 
-#include <R.h>
-#include <Rinternals.h>
+// basic math helpers
+#define MIN(A, B) ((A < B) ? (A) : (B))
+#define MAX(A, B) ((A > B) ? (A) : (B))
 
-// define some macros
+// R specific macros
 #define EXTRACT_NUMERIC_MATRIX(S_EXP, C_DATA, N_ROW, N_COL) \
     double *C_DATA = REAL(S_EXP); \
     const R_len_t N_ROW = nrows(S_EXP); \
@@ -17,11 +18,12 @@
 #define EXTRACT_INTEGER(S_EXP, I) \
     int I = INTEGER(S_EXP)[0];
 
+#define EXTRACT_REAL(S_EXP, D) \
+    double D = REAL(S_EXP)[0];
+
 #define ALLOC_VECTOR(type, size) (PROTECT(allocVector(type, size)))
 #define ALLOC_REAL_VECTOR(size) (ALLOC_VECTOR(REALSXP, size))
 #define ALLOC_INTEGER_VECTOR(size) (ALLOC_VECTOR(INTSXP, size))
 #define ALLOC_LIST(size) (ALLOC_VECTOR(VECSXP, size))
-
-
 
 #endif
