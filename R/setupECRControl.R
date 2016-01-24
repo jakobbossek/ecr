af360c7672091cba0c7fa98b384e13ba50136392
Jakob Bossek <info@jakobbossek.de>
2016-01-24 19:59:04 +0100

    use installMonitor in control and move transformFitness to dedicated file

--- R/setupECRControl.R
+++ R/setupECRControl.R
@@ -80,7 +80,7 @@ setupECRControl = function(
   }
 
   if (survival.strategy == "comma" && n.elite >= n.population) {
-     stopf("n.elite must be smaller than n.population! Otherwise each population would be the same.")
+    stopf("n.elite must be smaller than n.population! Otherwise each population would be the same.")
   }
 
   if (length(stopping.conditions) == 0) {
@@ -100,15 +100,11 @@ setupECRControl = function(
 
   event.dispatcher = setupEventDispatcher()
   if (!is.null(monitor)) {
-    event.dispatcher$registerAction("onEAInitialized", monitor$before)
-    event.dispatcher$registerAction("onPopulationUpdated", monitor$step)
-    event.dispatcher$registerAction("onEAFinished", monitor$after)
+    installMonitor(event.dispatcher, monitor)
   }
 
   if (!is.null(logger)) {
-    event.dispatcher$registerAction("onEAInitialized", logger$before)
-    event.dispatcher$registerAction("onPopulationUpdated", logger$step)
-    event.dispatcher$registerAction("onEAFinished", logger$after)
+    installMonitor(event.dispatcher, logger)
   }
 
   ctrl = makeS3Obj(
@@ -119,8 +115,6 @@ setupECRControl = function(
     survival.strategy = survival.strategy,
     n.elite = n.elite,
     stopping.conditions = stopping.conditions,
-    monitor = monitor,
-    logger = logger,
     custom.constants = custom.constants,
     vectorized.evaluation = vectorized.evaluation,
     event.dispatcher = event.dispatcher,
@@ -205,46 +199,4 @@ selectForSurvival = function(opt.state, population, control, n.select = control$
   fitness2 = transformFitness(fitness, task, control$survival.selector)
   idx.survival = control$survival.selector(fitness2, n.population, task, control, NULL)
   subsetPopulation(population, idx = idx.survival)
-}
-
-# @title
-# Fitness transformation / scaling.
-#
-# @description
-# Some selectors support maximization only, e.g., roulette wheel selector, or
-# minimization (most others). This function computes a factor from {-1, 1} for
-# each objective to match supported selector optimization directions and
-# the actual objectives of the task.
-#
-# @param fitness [matrix]
-#   Matrix of fitness values with the fitness vector of individual i in the i-th
-#   column.
-# @param task [ecr_optimization_task]
-#   Optimization task.
-# @param control [ecr_control]
-#   Control object.
-# @return [matrix] Transformed / scaled fitness matrix.
-transformFitness = function(fitness, task, selector) {
-  # logical vector of opt directions
-  task.dir = task$minimize
-  # "vectorize" character indicating supported opt direction by selector
-  sup.dir = rep(attr(selector, "supported.opt.direction"), task$n.objectives)
-  # "logicalize" selector opt direction
-  sup.dir = (sup.dir == "minimize")
-
-  fn.scale = ifelse(xor(task.dir, sup.dir), -1, 1)
-
-  # build transformation matrix
-  fn.scale = if (task$n.objectives == 1L) {
-    #FIXME: R BUG?!?!
-    # diag(ifelse(xor(task.dir, sup.dir), -1, 1)) breaks with message
-    # Fehler in diag(ifelse(xor(task.dir, sup.dir), -1, 1)) : ung"ultiger 'nrow' Wert (< 0)
-    # if n.objectives is 1! -.-
-    # Weird R bug??? diag(1) works!
-    as.matrix(fn.scale)
-  } else {
-    diag(fn.scale)
-  }
-  # transform fitness
-  return(fn.scale %*% fitness)
 }
