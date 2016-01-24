57abb8f51c6a61e9afb2962c8a55c3fc7884883f
Jakob Bossek <info@jakobbossek.de>
2016-01-24 20:22:49 +0100

    add optimization state getter functions

--- R/optState.R
+++ R/optState.R
@@ -1,3 +1,13 @@
+#' @title
+#' Optimization state.
+#'
+#' @description
+#' Description is coming soon ...
+#'
+#' @name OptState
+#' @rdname OptState
+NULL
+
 # @title Generate an optimization state object.
 #
 # @description The optimization task object is a container for all variables
@@ -14,6 +24,7 @@ setupOptState = function(task, population, control) {
   opt.state = new.env()
   opt.state$iter = 0L
   opt.state$time.created = Sys.time()
+  opt.state$time.passed = 0
   opt.state$task = task
   opt.state$par.set = task$par.set
   opt.state$n.evals = control$n.population
@@ -90,4 +101,87 @@ updateOptStateBestIndividual = function(opt.state) {
     }
   }
   invisible()
+}
+
+#' @title
+#' Optimization state getter functions.
+#'
+#' @description
+#' The \code{\link{OptState}} is generated internally by \code{\link{doTheEvolution}},
+#' but it is exposed to the user in some cases. E.g., self-defined actions or
+#' stopping conditions get the current \code{\link{OptState}} passed as an argument.
+#' The following collection of simple getter functions helps to access the
+#' properties of the optimization state.
+#'
+#' @param opt.state [\code{\link{OptState}}]\cr
+#'   Optimization state.
+#' @return [any] Type depends on the property.
+#' @name OptStateGetter
+#' @rdname OptStateGetter
+#' @export
+getOptStateCurrentIter = function(opt.state) {
+  return(opt.state$iter)
+}
+
+#' @rdname OptStateGetter
+#' @export
+getOptStateTask = function(opt.state) {
+  return(opt.state$task)
+}
+
+#' @rdname OptStateGetter
+#' @export
+getOptStateParamSet = function(opt.state) {
+  return(opt.state$par.set)
+}
+
+#' @rdname OptStateGetter
+#' @export
+getOptStateCurrentEvaluations = function(opt.state) {
+  return(opt.state$n.evals)
+}
+
+#' @rdname OptStateGetter
+#' @export
+getOptStateControl = function(opt.state) {
+  return(opt.state$control)
+}
+
+#' @rdname OptStateGetter
+#' @export
+getOptStateBestIndividual = function(opt.state) {
+  if (getOptStateTask(opt.state)$n.objectives > 1L) {
+    stopf("OptState: no 'best' individual in multi-objective optimization task.")
+  }
+  return(list(
+    param = opt.state$best.param,
+    value = opt.state$best.value
+  ))
+}
+
+#' @rdname OptStateGetter
+#' @export
+getOptStatePopulation = function(opt.state) {
+  return(opt.state$population)
+}
+
+#' @rdname OptStateGetter
+#' @export
+getOptStateFitness = function(opt.state) {
+  return(getOptStatePopulation(opt.state)$fitness)
+}
+
+#' @rdname OptStateGetter
+#' @export
+getOptStateTimePassed = function(opt.state) {
+  return(opt.state$time.passed)
+}
+
+#' @rdname OptStateGetter
+#' @export
+getOptStateOptPath = function(opt.state) {
+  if (is.null(opt.state$opt.path)) {
+    stopf("No ParamHelpers::OptPath available. You need to activate the OptPathLoggingMonitor.")
+  }
+  return(opt.state$opt.path)
 }
