36e758062c3b1590c3be8279dd55d712f13e73de
Jakob Bossek <info@jakobbossek.de>
2016-05-04 13:59:35 +0200

    export nomdomSelector

--- R/emoa.nsga2.R
+++ R/emoa.nsga2.R
@@ -48,52 +48,6 @@ nsga2 = function(
   max.time = NULL,
   ...) {
 
-  # set up the core of NSGA-II, namely the survival selection
-  nsga2SurvivalSelector = makeSelector(
-    selector = function(fitness, n.select, task, control, opt.state) {
-      nondom.layers = doNondominatedSorting(fitness)
-
-      # storage for indizes of selected individuals
-      new.pop.idxs = integer()
-
-      # get maximum rank, i.e., the number of domination layers
-      max.rank = max(nondom.layers$ranks)
-
-      # get the indizes of points for each domination layer
-      idxs.by.rank = lapply(seq(max.rank), function(r) which(nondom.layers$ranks == r))
-
-      # get the number of points in each domination layer ...
-      front.len = sapply(idxs.by.rank, length)
-
-      # ... cumulate the number of points of the domination layers ...
-      cum.front.len = cumsum(front.len)
-
-      # ... and determine the first domination layer, which does not fit as a whole
-      front.first.nonfit = which.first(cum.front.len > n.select)
-
-      if (front.first.nonfit > 1L) {
-        # in this case at least one nondominated front can be added
-        new.pop.idxs = unlist(idxs.by.rank[1:(front.first.nonfit - 1L)])
-      }
-
-      # how many points to select by second criterion, i.e., crowding distance?
-      n.diff = n.select - length(new.pop.idxs)
-
-      if (n.diff > 0L) {
-        idxs.first.nonfit = idxs.by.rank[[front.first.nonfit]]
-        cds = computeCrowdingDistance(fitness[, idxs.first.nonfit, drop = FALSE])
-        idxs2 = order(cds, decreasing = TRUE)[1:n.diff]
-        new.pop.idxs = c(new.pop.idxs, idxs.first.nonfit[idxs2])
-      }
-
-      # merge the stuff and return
-      return(new.pop.idxs)
-    },
-    supported.objectives = "multi-objective",
-    name = "NSGA-II survival selector",
-    description = "nondominated sorting with potential crowding distance"
-  )
-
   if (isSmoofFunction(task)) {
     task = makeOptimizationTask(task)
   }
@@ -116,7 +70,7 @@ nsga2 = function(
     parent.selector = parent.selector,
     recombinator = recombinator,
     mutator = mutator,
-    survival.selector = nsga2SurvivalSelector
+    survival.selector = setupNondomSelector()
   )
 
   return(doTheEvolution(task, ctrl))
